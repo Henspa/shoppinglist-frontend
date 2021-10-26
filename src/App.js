cffbaa0e1ccfaa638ea07c2f7bd9f02ca4c18001
henni saaranen <n0sahe00@students.oamk.fi>
2021-10-26 17:00:20 +0300

    projekti aloitettu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+
+const URL = 'http://localhost/shoppinglist/index.php';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios.get(URL)
+    .then((response) => {
+      console.log(response.data);
+      setItems(response.data);
+    }).catch(error => {
+      alert(error);
+    })
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ol>
+      {items?.map(item => (
+        <li key={item.id}>{item.description}</li>
+      ))}
+    </ol>
   );
 }
 
