72494488fadaa0306ca326d92cda92891957e9cb
henni saaranen <n0sahe00@students.oamk.fi>
2021-10-27 12:44:44 +0300

    lisäys ja poisto tökkii

--- src/App.js
+++ src/App.js
@@ -2,28 +2,77 @@ import {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
 
-const URL = 'http://localhost/shoppinglist/index.php';
+const URL = 'http://localhost/shoppinglist/';
 
 function App() {
   const [items, setItems] = useState([]);
+  const [item, setItem] = useState('type description');
+  const [amount, setAmount] = useState('type amount');
 
   useEffect(() => {
     axios.get(URL)
     .then((response) => {
-      console.log(response.data);
+      //console.log(response.data);
       setItems(response.data);
+      setItem(response.data);
+      setAmount(response.data);
     }).catch(error => {
-      alert(error);
+      alert(error.response ? error.response.data.error : error);
     })
   }, [])
 
+  function add(e) {
+    e.preventDefault();
+    const json = JSON.stringify({description:item})
+    axios.post(URL + 'save.php',json,{
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+      .then((response) => {
+        setItems(items => [...items, response.data]);
+        setItem('');
+        setAmount('');
+      }).catch (error => {
+        alert(error.response.data.error)
+      });
+  }
+
+  function remove(id) {
+    const json = JSON.stringify({id:id})
+    axios.post(URL + 'delete.php',json,{
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then((response) => {
+      const newListWithoutRemoved = items.filter((item) => item.id !== id);
+      setItems(newListWithoutRemoved);
+    }).catch (error => {
+      alert(error.response ? error.response.data.error : error);
+    });
+  }
 
   return (
-    <ol>
-      {items?.map(item => (
-        <li key={item.id}>{item.description}</li>
-      ))}
-    </ol>
+    <div className="container">
+      <h3>Shopping List</h3>
+      <form onSubmit={add}>
+        <label>New item</label>
+        <input value={item} onChange={e => setItems(e.target.value)} />
+        <input value={amount} onChange={e => setAmount(e.target.value)} />
+        <button>Add</button>
+      </form>
+      <ol>
+        {items?.map(item => (
+          <li key={item.id}>
+            {item.description}&nbsp;
+            <a href="#" className="delete" onClick={() => remove(item.id)}>
+              Delete
+            </a>
+          </li>
+        ))}
+      </ol>
+    </div>
   );
 }
 
